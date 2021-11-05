bfc4bb48c750e5000887e321c5da2f3a022dc4ee
SomeoneSKtttz7 <53940106+SomeoneSK@users.noreply.github.com>
2021-11-05 21:03:16 +0000

    moved help to commands, timeline_graph function

--- helper_functions/charts.js
+++ helper_functions/charts.js
@@ -1,69 +1,83 @@
 const QuickChart = require('quickchart-js');
+const moment = require('moment');
+moment().format(); 
 
-//await interaction.reply( {content:'test!', files: [image]} )
-async function line_chart() {
+async function timeline_graph(data={}, title = "", x_label = "", y_label = "") {
+	let dataset = {
+		label: "",
+		backgroundColor: "rgba(75, 192, 192, 0.5)",
+		borderColor: "rgb(75, 192, 192)",
+		fill: false,
+		data: []
+	}
+	for (let key of Object.keys(data)) {
+		dataset.data.push( {x: moment(parseInt(key)), y: data[key] } )
+	}
+
+	let options = {
+		title: {
+			display: true,
+			text: title,
+			fontColor: 'white',
+		},
+		legend: {
+			display: false
+		},
+		scales: {
+			xAxes: [{
+				type: "time",
+				//time: {parser: "MM/DD/YYYY HH:mm",},
+				scaleLabel: {
+					display: true,
+					labelString: x_label,
+					fontColor: 'white',
+				},
+				ticks: {
+					fontColor: 'white',
+				},
+				gridLines: {
+					color: 'rgb(143, 143, 143)',
+					zeroLineColor: 'rgb(143, 143, 143)',
+				}
+			}],
+			yAxes: [{
+				scaleLabel: {
+					display: true,
+					labelString: y_label,
+					fontColor: 'white',
+				},
+				ticks: {
+					fontColor: 'white',
+				},
+				gridLines: {
+					color: 'rgb(143, 143, 143)',
+					zeroLineColor: 'rgb(143, 143, 143)',
+				}
+			}]
+		}
+	}
+
+	if (title === "") {options.title.display = false}
+	if (x_label === "") {options.scales.xAxes[0].scaleLabel.display = false}
+	if (y_label === "") {options.scales.yAxes[0].scaleLabel.display = false}
+
 	const qc = new QuickChart();
 	qc.setConfig({
 	type: 'line',
 	data: {
 		labels: [],
-		datasets: [
-			{
-				"label": "Dataset with point data",
-				"backgroundColor": "rgba(75, 192, 192, 0.5)",
-				"borderColor": "rgb(75, 192, 192)",
-				"fill": false,
-				"data": [
-				{
-					"x": "06/14/2020 09:08",
-					"y": -29
-				},
-				{
-					"x": "06/19/2020 09:08",
-					"y": -34
-				},
-				{
-					"x": "06/21/2020 09:08",
-					"y": -62
-				},
-				{
-					"x": "06/29/2020 19:08",
-					"y": 1
-				}
-				]
-			},
-		],
+		datasets: [dataset],
 	},
-	options: {
-		"scales": {
-			"xAxes": [{
-				"type": "time",
-				"time": {
-				"parser": "MM/DD/YYYY HH:mm",
-				},
-				"scaleLabel": {
-				"display": true,
-				"labelString": "Date"
-				}
-			}],
-			"yAxes": [{
-				"scaleLabel": {
-				"display": true,
-				"labelString": "value"
-				}
-			}]
-		}
-	},
+	options: options,
 	});
-	qc.setWidth(500).setHeight(300).setBackgroundColor('transparent');
+	qc.setWidth(600).setHeight(400).setBackgroundColor('transparent');
 
 	const image = await qc.toBinary();
-	qc.toFile('/tmp/mychart.png');
 	return image
 }
 
 const Charts_functions = {
-	line_chart: line_chart,
+	timeline_graph: timeline_graph,
 }
 
 exports.Charts_functions = Charts_functions;
--- test_commands/test.js
+++ test_commands/test.js
@@ -7,7 +7,7 @@ module.exports = {
 		.setName('test')
 		.setDescription('test!'),
 	async execute(interaction) {
-		let img = Charts_functions.line_chart()
+		let img = await Charts_functions.timeline_graph(data={1636145940:10, 1636145941:12, 1636145942:9, 1636145948:3, 1636145980:100}, title = "Test Graph", x_label = "", y_label = "Values")
 		await interaction.reply( {content:'test!', files: [img] } );
 	},
 }
