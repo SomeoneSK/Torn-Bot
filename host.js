2fcda0a8818488ba46cd88d16c50f22fb0e58bfe
SomeoneSK <53940106+SomeoneSK@users.noreply.github.com>
2021-12-09 21:36:34 +0100

    Update host.js

--- host.js
+++ host.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 function host() {
 	app.get('/', (req, res) => res.send('Hello World!'));
