d0e9bc69cfb996f9caf56e50cbb989bf9d03a6e9
SomeoneSK <53940106+SomeoneSK@users.noreply.github.com>
2021-12-17 14:57:54 +0100

    Update graph_stock.js

--- message_constructors/graphs/graph_stock.js
+++ message_constructors/graphs/graph_stock.js
@@ -1,6 +1,5 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { MessageActionRow, MessageButton, MessageEmbed, MessageSelectMenu, MessageAttachment } from 'discord.js';
-import {General_functions} from "../../helper_functions/general.js"
 import {Components_functions} from '../../helper_functions/components.js'
 import {Torn_data} from '../../torn/index.js'
 import {Embed_functions} from '../../helper_functions/embeds.js'
@@ -12,28 +11,11 @@ async function graph_stock(interaction, stock_acronym, property, interval) {
 		return false
 	}
   
-	let data = await General_functions.http_request( 'https://tornsy.com/api/' +  stock_acronym + '?interval=' + interval)
-
-	let formatted_data = {}
-	let index = 0
-	for (let one of data["data"]) {
-    let values = {}
-		values.current_price = one[1]
-    values.total_shares = one[2]
-    if (interval !== 'm1') {
-      values.total_shares = one[5]
-      values.high = one[2]
-      values.low = one[3]
-      values. close = one[4]
-    }
-    values.market_cap = values.current_price * values.total_shares
-		formatted_data[ one[0]*1000 ] = values[property]
-	}
+  let graph = await Graphs_functions.stock(stock_acronym, property, interval)
   const property_capitalized = property.charAt(0).toUpperCase() + property.slice(1);
   const title = property_capitalized.replace("_", " ") + " of " + stock_acronym.toUpperCase() + " with interval " + interval
-	let graph = await Graphs_functions.timeline_graph(formatted_data, title, property.replace("_", " "))
   
-  const attachment = new MessageAttachment(graph,'graph.png'); 
+  const attachment = new MessageAttachment(graph,'graph.png');
 	const embed = new MessageEmbed()
     .setColor('#0099ff')
     .setTitle( title )
